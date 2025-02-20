d48029bee072bfcd9b195cad0155991302fc3bfe
seven-beto <herberthfreitas26@gmail.com>
2025-02-20 00:02:19 -0300

    Componente Status adicionado

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History () {
     return (
@@ -21,43 +21,57 @@ export function History () {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Ha 2 meses</td>
-                            <td>Concluido</td>
+                            <td>
+                                <Status statusColor="green">Concluido</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
